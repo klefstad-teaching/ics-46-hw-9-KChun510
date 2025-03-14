c5c42361c350ad812f128285f254d501490a717f
Kchun510 <kylechun1@gmail.com>
2025-03-14 12:40:46 -0700

    yippe

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -121,9 +121,7 @@ void print_word_ladder(const vector<string>& ladder) {
     }
     cout << "Word ladder found: ";
     for (size_t i = 0; i < ladder.size(); i++) {
-        if (i > 0){
             cout << ladder[i] << " ";
-        }
     }
     cout << endl;
 }
